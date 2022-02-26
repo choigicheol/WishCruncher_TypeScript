d10a3e1ced39fbfeebf4879d3fcaf91374898881
choigicheol <kofpo900916@gmail.com>
2022-02-26 19:40:27 +0900

    [Client][LoginPage] 로그인페이지 레이아웃 구현

--- client/src/Pages/LoginPage/LoginPage.style.ts
+++ client/src/Pages/LoginPage/LoginPage.style.ts
@@ -1,1 +1,107 @@
 import styled from "styled-components";
+
+interface ButtonRole {
+  role: string;
+}
+
+export const LoginBox = styled.div`
+  max-width: 475px;
+  width: 100%;
+  height: 583px;
+  background: #ffffff;
+  box-shadow: 0px 0px 11px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin: 60px 0 60px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  form {
+    width: 100%;
+  }
+  @media screen and (max-width: 590px) {
+    padding: 20px;
+    margin: 30px 0 30px 0;
+    height: 493px;
+  }
+`;
+
+export const Title = styled.div`
+  font-size: 32px;
+  width: 100%;
+  margin: 20px 0 40px 0;
+  @media screen and (max-width: 590px) {
+    font-size: 24px;
+    margin: 20px 0 20px 0;
+  }
+`;
+
+export const LoginInput = styled.input`
+  width: 100%;
+  height: 47px;
+  background: #fbfbfb;
+  border: 2px solid #dedede;
+  box-sizing: border-box;
+  border-radius: 4px;
+  margin: 6px 0 43px 0;
+  font-weight: 300;
+  padding: 0 10px 0 10px;
+  color: #2d2d2d;
+  @media screen and (max-width: 590px) {
+    height: 37px;
+    font-size: 13px;
+  }
+`;
+
+export const Button = styled.button<ButtonRole>`
+  width: 100%;
+  height: 47px;
+  line-height: 47px;
+  border: ${(props) => (props.role === "kakao" ? "1px solid #e0dde1" : "none")};
+  box-sizing: border-box;
+  border-radius: 4px;
+  margin-bottom: ${(props) => (props.role === "kakao" ? "48px" : "21px")};
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.12);
+  color: ${(props) => (props.role === "kakao" ? "none" : "#ffffff")};
+  background: ${(props) => (props.role === "kakao" ? "#ffffff" : "#ab86df")};
+  font-size: 16px;
+  cursor: pointer;
+  &:active {
+    background: #8b6aba;
+  }
+  @media screen and (max-width: 590px) {
+    height: 37px;
+    font-size: 14px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const KakaoBtnContent = styled.div`
+  width: 100%;
+  height: 43px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    margin-right: 5px;
+  }
+  @media screen and (max-width: 590px) {
+    height: 34px;
+    font-size: 14px;
+  }
+`;
+
+export const WarningMessage = styled.div`
+  color: rgb(255, 55, 55);
+  visibility: hidden;
+  width: 100%;
+  height: 68px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 590px) {
+    height: 48px;
+    font-size: 14px;
+    margin-bottom: 15px;
+  }
+`;
--- client/src/Pages/LoginPage/LoginPage.tsx
+++ client/src/Pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  LoginBox,
+  Title,
+  LoginInput,
+  Button,
+  WarningMessage,
+  KakaoBtnContent,
+} from "./LoginPage.style";
 
 function LoginPage() {
-  return <div>로그인페이지</div>;
+  return (
+    <>
+      <LoginBox>
+        <form>
+          <Title>로그인</Title>
+          <label>이메일</label>
+          <LoginInput type="email" name="email_input" required />
+          <label>비밀번호</label>
+          <LoginInput type="password" name="password_input" required />
+          <WarningMessage>로그인 정보를 확인해주세요</WarningMessage>
+          <Button role="">로그인</Button>
+        </form>
+        <Button role="kakao">
+          <KakaoBtnContent>
+            <img src="./kakaoLogo.svg" />
+            <span>카카오 로그인</span>
+          </KakaoBtnContent>
+        </Button>
+        <div>
+          <Link to="/signup">아직 이메일이 없으신가요? 회원가입 하러가기</Link>
+        </div>
+      </LoginBox>
+    </>
+  );
 }
 
 export default LoginPage;
